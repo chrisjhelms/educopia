7110da4b8384645ab8003227f327e0eb21eb5c26
momeven <momeven@gmail.com>
2012-05-18 15:54:30 +0000

    add params to caches.py to enable listing of url, nickname, domainname

--- pln_admin/lockssview/caches.py
+++ pln_admin/lockssview/caches.py
@@ -15,17 +15,48 @@ from django.db import models
 from status.models import LockssCache; 
 
 class StatusScript(LockssScript):
-    ''' list all known caches  '''
+    ''' list all known caches, without parameters print nickname and domainname '''
     
+    CONFIGURATION_DEFAULTS = {
+                'nickname': False,
+                "dnsname":  False,  
+                "url": False
+                }; 
+                
     def __init__(self, argv0):
-        LockssScript.__init__(self, argv0, '$Revision: $', {}) 
+        LockssScript.__init__(self, argv0, '$Revision: $', StatusScript.CONFIGURATION_DEFAULTS) 
         
     def _create_opt_parser(self):
-        return LockssScript._create_parser(self, au_params=False, mayHaveServer=False, credentials=False) 
-        
+        option_parser =  LockssScript._create_parser(self, au_params=False, mayHaveServer=False, credentials=False) 
+        option_parser.add_option('-n', '--nickname',
+                                  action='store_true',
+                                  help="print cache's nickname [%default]")
+        option_parser.add_option('-D', '--dnsname',
+                                  action='store_true',
+                                  help="print cache's dnsname [%default]")
+        option_parser.add_option('-u', '--url',
+                                  action='store_true',
+                                  help="print cache's login url [%default]")
+        return option_parser
+    
     def process(self):
+        if ( not self.options.nickname   and 
+             not self.options.dnsname and 
+             not self.options.url ) : 
+            self.options.dnsname = True; 
+            self.options.nickname = True; 
+            
         for c in LockssCache.objects.all(): 
-            print c
+            if (self.options.nickname): 
+                print c.name, "\t",
+            if (self.options.dnsname): 
+                print c.domain, "\t",
+            if (self.options.url): 
+                print "http//%s:%d" %  (c.domain, c.port), 
+            print ""
+            
+                
+                
 
 def __main(): 
     global script
