45d8c75fb0ffa2bd6769cfc94893136881b5a30e
momeven <momeven@eeb50289-4028-b381-a82c-9f05b47cec8e>
2012-10-05 18:12:53 +0000

    add types to admin interface

--- trunk/pln_admin/lockssview/lockssview/admin.py
+++ trunk/pln_admin/lockssview/lockssview/admin.py
@@ -6,6 +6,11 @@ from lockssview import *;
 
 admin.site.register(LockssCache)
 admin.site.register(MasterAuId)
+admin.site.register(LockssCacheAuId)
+admin.site.register(LockssCrawlStatus)
+admin.site.register(UrlReport)
+admin.site.register(Url)
 admin.site.register(CacheCommPeer)
+admin.site.register(RepositorySpace)
 
 
