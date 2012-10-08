0a0c3fa848f0958798b6b593d1a7ab04070eecb1
momeven <momeven@gmail.com>
2012-10-08 21:08:56 +0000

    custom views - very simple

--- pln_admin/lockssview/lockssview/views/caches.py
+++ pln_admin/lockssview/lockssview/views/caches.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from lockssview.models import LockssCache; 
 
 def index(request):
     lst  = LockssCache.objects.all(); 
-    return render_to_response('generic/index.html', {'url' : 'caches', 'lst': lst})
+    return render_to_response('lockssview/caches/index.html', 
+                              {'url' : '/lockssview/caches', 
+                               'setname' : 'all', 'lst': lst})
+
+def network(request,network):
+    lst  = LockssCache.objects.filter(network=network); 
+    return render_to_response('lockssview/caches/index.html', 
+                              {'url' : '/lockssview/caches', 
+                               'setname': network,'lst': lst})
+    
+def detail(request,obj_id):
+    obj =  get_object_or_404(LockssCache, pk=obj_id); 
+    return render_to_response('lockssview/caches/detail.html', {'obj': obj})
+    
