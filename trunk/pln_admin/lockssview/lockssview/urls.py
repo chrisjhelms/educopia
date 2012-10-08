af7388cc1c594265ad2d290baf66ce88df667429
momeven@gmail.com <momeven@gmail.com@eeb50289-4028-b381-a82c-9f05b47cec8e>
2012-10-08 21:08:56 +0000

    custom views - very simple

--- trunk/pln_admin/lockssview/lockssview/urls.py
+++ trunk/pln_admin/lockssview/lockssview/urls.py
@@ -11,7 +11,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'lockssview.views.home', name='home'),
     url(r'^caches$', 'lockssview.views.caches.index'),
-
+    url(r'^caches/(?P<obj_id>\d+)/$', 'lockssview.views.caches.detail'),
+    url(r'^networks/(?P<network>\w+)/$', 'lockssview.views.caches.network'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
