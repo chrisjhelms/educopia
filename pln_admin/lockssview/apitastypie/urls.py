a0e3405f44abf94c844e611f825be5e5faa15079
momeven <momeven@gmail.com>
2012-10-12 17:16:09 +0000

    tastypie api

---
+++ pln_admin/lockssview/apitastypie/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import *
+from tastypie.api import Api;
+from apitastypie.api import *;
+
+api = Api(api_name='')
+api.register( LockssCacheResource() );
+api.register( MasterAuIdResource() );
+api.register( LockssCacheAuIdResource() );
+api.register( LockssCacheAuSummaryResource() );
+api.register( LockssCrawlStatusResource() );
+api.register( UrlReportResource() );
+api.register( CacheCommPeerResource() );
+api.register( RepositorySpaceResource() );
+
+urlpatterns = patterns('',
+    # The normal jazz here...
+    (r'^v1', include(api.urls)),
+)
