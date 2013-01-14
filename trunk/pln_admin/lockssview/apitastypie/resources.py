ebe277de2552c2b9510a0da0f2dccce5b8617c8f
momeven@gmail.com <momeven@gmail.com@eeb50289-4028-b381-a82c-9f05b47cec8e>
2013-01-14 16:22:31 +0000

    fix indent

--- trunk/pln_admin/lockssview/apitastypie/resources.py
+++ trunk/pln_admin/lockssview/apitastypie/resources.py
@@ -11,7 +11,6 @@ class GetPostMeta:
     #list_allowed_methods = ['get', 'post']
     #detail_allowed_methods = ['get', 'post']
     allowed_methods = ['get', 'post']
-    authentication = BasicAuthentication()
     serializer = Serializer(formats=['json', 'xml'])
 
 
@@ -25,37 +24,48 @@ class LockssCacheResource(ModelResource):
         resource_name = 'caches'
         
     def dehydrate(self, bundle):
-        bundle.data['nauids'] = bundle.obj.locksscacheauid_set.count()
+        bundle.data['nauIds'] = bundle.obj.locksscacheauid_set.count()
         return bundle
-        
+
 class MasterAuIdResource(ModelResource):
+    cacheAuIds = fields.OneToManyField('apitastypie.resources.LockssCacheAuIdResource', 'locksscacheauid_set'); 
+
     class Meta(GetPostMeta):
         queryset = MasterAuId.objects.all()
-        filtering = { 'auId' : ('startswith', ) };
+        filtering = { #'auId' :    ( 'startswith', 'exact' ),   '|' => or 
+                      #'plugin' :  ( 'startswith', 'exact' ),
+                      'baseUrl' : ( 'startswith', 'exact' )  };
         resource_name = 'master_auids'
     
     def dehydrate(self, bundle):
-        bundle.data['repl'] = bundle.obj.locksscacheauid_set.count()
+        qu =  bundle.obj.locksscacheauid_set;
+        #bundle.data['cacheAuIds'] = qu.all(); 
+        bundle.data['repl'] = qu.count()
         return bundle
     
 class LockssCacheAuIdResource(ModelResource):
-    masterAuId = fields.ForeignKey(MasterAuIdResource, 'masterAuId'); 
+    master_auId = fields.ForeignKey(MasterAuIdResource, 'masterAuId'); 
     cache = fields.ForeignKey(LockssCacheResource, 'cache'); 
-
+    crawlStatus = fields.OneToManyField('apitastypie.resources.LockssCrawlStatusResource', 
+                                        'locksscrawlstatus_set'); 
+    cache_au_summary = fields.OneToOneField('apitastypie.resources.LockssCacheAuSummaryResource', 
+                                       'locksscacheausummary'); 
     class Meta(GetPostMeta):
         queryset = LockssCacheAuId.objects.all()
         filtering = { 'cache_id' : ('exact', ), 
                       'auId' : ('exact', 'startswith', )} 
         resource_name = 'cache_auids'
-
+        
     cache = fields.ToOneField('apitastypie.resources.LockssCache', 'cache'); 
 
 class LockssCacheAuSummaryResource(ModelResource):
-    auId = fields.ForeignKey(LockssCacheAuIdResource, 'auId'); 
+   auId = fields.ForeignKey(LockssCacheAuIdResource, 'auId'); 
 
-    class Meta(GetPostMeta):
-        queryset = LockssCacheAuSummary.objects.all()
-        filtering = { 'agreement' : ('lt', 'gt', )} 
+   class Meta(GetPostMeta):
+	queryset = LockssCacheAuSummary.objects.all()
+        filtering = { 'agreement' : ('lt', 'lte', 'gt', 'gte' ),
+                      'reportDate' : ( 'exact', 'lt', 'lte', 'gt', 'gte' ) 
+                     } 
         resource_name = 'cache_au_summaries'
 
 class LockssCrawlStatusResource(ModelResource):
@@ -63,6 +73,9 @@ class LockssCrawlStatusResource(ModelResource):
 
     class Meta(GetPostMeta):
         queryset = LockssCrawlStatus.objects.all()
+        filtering = { 'status' : ('exact' ),
+                      'reportDate' : ( 'exact', 'lt', 'lte', 'gt', 'gte' ) 
+	};
         resource_name = 'au_crawl_status'
 
 class UrlReportResource(ModelResource):
@@ -70,16 +83,19 @@ class UrlReportResource(ModelResource):
 
     class Meta(GetPostMeta):
         queryset = UrlReport.objects.all()
+        filtering = { 'reportDate' : ( 'exact', 'lt', 'lte', 'gt', 'gte' ) };
         resource_name = 'cache_au_url_report'
 
 class CacheCommPeerResource(ModelResource):
     class Meta(GetPostMeta):
         queryset = CacheCommPeer.objects.all()
+        filtering = { 'reportDate' : ( 'exact', 'lt', 'lte', 'gt', 'gte' ) };
         resource_name = 'cache_comm_peer'
 
 class RepositorySpaceResource(ModelResource):
     class Meta(GetPostMeta):
         queryset = RepositorySpace.objects.all()
+        filtering = { 'reportDate' : ( 'exact', 'lt', 'lte', 'gt', 'gte' ) };
         resource_name = 'cache_repos_space'
 
 
